37e8c9e5b06ef2ea16ca61e79f04cf7cf15157c1
VadimJedi <108vadim@mail.ru>
2018-04-23 22:54:18 +0800

    init

---
+++ main.go
@@ -0,0 +1,61 @@
+//
+// Реализовать чтение с нескольких каналов
+// ps: программа корректно работает в режиме гонки -race
+//
+
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// Канал с которого бум читать, принимает на вход числа
+// Таких каналов можно создать множество
+// Например они что-то вычисляют паралелньно и отправляют в канал
+func SomeChannel(nums ...int) <-chan int {
+	outChannel := make(chan int)
+	go func() {
+		for _, num := range nums {
+			outChannel <- num * 2
+		}
+		close(outChannel)
+	}()
+	return outChannel
+}
+
+// Функция чтения с каналов
+// На вход получаем каналы, и отдает в один канал подсчитанные данные
+func ReadFromChannels(channels ...<-chan int) <-chan int {
+	var vg sync.WaitGroup
+	vg.Add(len(channels))
+	outChannel := make(chan int)
+	outPutWorkers := func(in <-chan int) {
+		for ch := range in {
+			outChannel <- ch
+		}
+		vg.Done()
+	}
+
+	for _, ch := range channels {
+		go outPutWorkers(ch)
+	}
+
+	go func() {
+		vg.Wait()
+		close(outChannel)
+	}()
+
+	return outChannel
+}
+
+func main() {
+	oneChannel := SomeChannel(1, 2, 3, 4, 5)
+	twoChannel := SomeChannel(100, 200)
+	threeChannel := SomeChannel(1000, 2000)
+
+	// Передаем в функцию наши каналы и печатаем результат из одного канала
+	for n := range ReadFromChannels(oneChannel, twoChannel, threeChannel) {
+		fmt.Println(n)
+	}
+}
